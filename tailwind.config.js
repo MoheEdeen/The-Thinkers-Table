c1ac505ab2e7ded67364ad81de35f6b863928c8e
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2025-05-04 13:24:21 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                thinkerGold: "#E4B545",
+            },
+        },
+    },
+    plugins: [],
+};
